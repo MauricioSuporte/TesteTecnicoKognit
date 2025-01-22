34b8211066a88c90e19a87365c0820e392792a46
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-01-22 18:26:17 -0300

    Add project files.

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace UserWalletAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Wallet.cs
@@ -0,0 +1,13 @@
+﻿namespace UserWalletAPI.Models
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public decimal ValorAtual { get; set; }
+        public string Banco { get; set; }
+        public DateTime UltimaAtualizacao { get; set; }
+
+        public User User { get; set; }
+    }
+}
