cc4a0d73f99921ade9dd65b382b372f4fb59915b
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-01-22 18:51:58 -0300

    Add exemples apis for User and Wallet

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserWalletAPI.Models;
+
+namespace UserWalletAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly List<User> Users = [];
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User user)
+        {
+            user.Id = Users.Count + 1;
+            Users.Add(user);
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            var user = Users.FirstOrDefault(u => u.Id == id);
+            return user is null ? NotFound() : Ok(user);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(Users);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/WalletController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserWalletAPI.Models;
+
+namespace UserWalletAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WalletController : ControllerBase
+    {
+        private static readonly List<Wallet> Wallets = [];
+
+        [HttpPost]
+        public IActionResult CreateWallet([FromBody] Wallet wallet)
+        {
+            wallet.Id = Wallets.Count + 1;
+            wallet.UltimaAtualizacao = DateTime.Now;
+            Wallets.Add(wallet);
+            return CreatedAtAction(nameof(GetWallet), new { id = wallet.Id }, wallet);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetWallet(int id)
+        {
+            var wallet = Wallets.FirstOrDefault(w => w.Id == id);
+            return wallet is null ? NotFound() : Ok(wallet);
+        }
+
+        [HttpGet("user/{userId}")]
+        public IActionResult GetWalletsByUserId(int userId)
+        {
+            var userWallets = Wallets.Where(w => w.UserId == userId).ToList();
+            return Ok(userWallets);
+        }
+    }
+}
