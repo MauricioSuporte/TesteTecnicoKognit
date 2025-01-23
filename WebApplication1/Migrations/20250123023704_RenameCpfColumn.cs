45ca4804b8112cc604a26037ab9aa7e96aee0094
Maurício Bernardo <mauriciobernardodesouza2@hotmail.com>
2025-01-22 23:39:35 -0300

    Add migration RenameCpfColumn

---
+++ WebApplication1/Migrations/20250123023704_RenameCpfColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserWalletAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameCpfColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CPF",
+                table: "Users",
+                newName: "Cpf");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Cpf",
+                table: "Users",
+                newName: "CPF");
+        }
+    }
+}
